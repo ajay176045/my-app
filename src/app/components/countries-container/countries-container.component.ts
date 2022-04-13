9d619a9fe540dda002ed2526c3954ded3feda002
Ajay Singh Thakur <ajaysingh.thakur@kbc.be>
2022-04-13 19:22:21 +0200

    first commit

---
+++ src/app/components/countries-container/countries-container.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-countries-container',
+  templateUrl: './countries-container.component.html',
+  styleUrls: ['./countries-container.component.scss']
+})
+export class CountriesContainerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
